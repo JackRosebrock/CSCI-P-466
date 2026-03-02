a81caf05818d1fc92a5d5967e0919d4e3623e87b
jrosebr <jrosebr@iu.edu>
2026-03-02 18:40:41 -0500

    Created the first functional version of my website

--- Project-1/src/main/java/com/example/Project1/config/SecurityConfig.java
+++ Project-1/src/main/java/com/example/Project1/config/SecurityConfig.java
@@ -3,8 +3,47 @@ package com.example.Project1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 public class SecurityConfig 
 {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+            )
 
+            .formLogin(form -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/home", true)
+                .permitAll()
+            )
+
+            .logout(Customizer.withDefaults())
+            .build();
+    }
+
+
+    @Bean
+    UserDetailsService userDetailsService()
+    {
+        UserDetails student = User.withDefaultPasswordEncoder()
+            .username("student")
+            .password("password")
+            .roles("USER")
+            .build();
+
+        
+        return new InMemoryUserDetailsManager(student);
+    }
 }
--- Project-1/src/main/java/com/example/Project1/controllers/PageController.java
+++ Project-1/src/main/java/com/example/Project1/controllers/PageController.java
@@ -23,5 +23,24 @@ public class PageController
     {
         return "login";
     }
-    
+
+
+    @GetMapping("/home")
+    public String home(Model model, Principal principal)
+    {
+        model.addAttribute("username", principal.getName());
+        return "home";
+    }
+
+    @GetMapping("/schedule")
+    public String schedule()
+    {
+        return "schedule";
+    }
+
+    @GetMapping("/videoButton")
+    public String videoButton()
+    {
+        return "videoButton";
+    }
 }
